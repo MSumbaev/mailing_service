35df0b4d136a04a2807503dab52779fd5f52c507
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-01 21:31:52 +0300

    created model User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from mailing.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    phone = models.CharField(max_length=30, verbose_name='Телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    telegram = models.CharField(max_length=100, verbose_name='Telegram', **NULLABLE)
+    verify_key = models.CharField(max_length=15, verbose_name='Ключ верификации', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
