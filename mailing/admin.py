9dc9bae18d3ed1185e94e0d8514199d5f458efcb
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-01 16:39:33 +0300

    Добавление Client, MailingMessage и MailingSettings в админку

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import Client, MailingMessage, MailingSettings
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name',)
+
+
+@admin.register(MailingMessage)
+class MailingMessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'subject', 'message')
+    search_fields = ('subject',)
+
+
+@admin.register(MailingSettings)
+class MailingSettingsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'message', 'period', 'start_time', 'end_time', 'status')
+    search_fields = ('clients', 'message',)
