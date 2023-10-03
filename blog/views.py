85c1241aedcbe730351c7753b3d5b09798f3588d
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-03 23:21:13 +0300

    созданы контроллеры BlogListView и BlogDetailView, созданы шаблоны для контроллеров, прописаны пути в urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Blog
+
+
+class BlogListView(ListView):
+    model = Blog
+    extra_context = {
+        'title': 'Статьи'
+    }
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    extra_context = {
+        'title': 'Статьи'
+    }
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+
+        return self.object
