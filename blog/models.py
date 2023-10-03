b0be74d7bcd265289978e33ebed0fd760aabfae4
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-10-03 22:00:56 +0300

    созданы модель Blog и urls.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from mailing.models import NULLABLE
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Содержимое статьи')
+    image = models.ImageField(upload_to='blog/', verbose_name='Изображение', **NULLABLE)
+    view_count = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
+    published_at = models.DateField(auto_now_add=True, verbose_name='Дата публикации')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django_crontab',
     'mailing',
     'users',
+    'blog'
 ]
 
 MIDDLEWARE = [
